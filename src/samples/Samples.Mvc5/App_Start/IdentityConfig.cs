d3b5c9fb788d8eb60cfa169e26adde3cf3584a83
Daniel Wertheim <daniel@wertheim.se>
2014-10-28 11:11:56 +0100

    Add missing pieces from newly generated sample.

--- src/samples/Samples.Mvc5/App_Start/IdentityConfig.cs
+++ src/samples/Samples.Mvc5/App_Start/IdentityConfig.cs
@@ -110,4 +110,22 @@ namespace Samples.Mvc5
             return Task.FromResult(0);
         }
     }
+
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
 }
